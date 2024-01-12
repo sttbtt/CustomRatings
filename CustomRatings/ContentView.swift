bd990b3d74a466da27fb2547b603520e6b54254a
Scott Bennett <stt.btt@gmail.com>
2024-01-12 17:25:33 -0500

    Initial Commit

---
+++ CustomRatings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomRatings
+//
+//  Created by Scott Bennett on 1/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomRatings/CustomRatingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomRatingsApp.swift
+//  CustomRatings
+//
+//  Created by Scott Bennett on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomRatingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
