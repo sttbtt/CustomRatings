cf5b267ff70fe7a7c50a6ad1a68033762f8378b6
Scott Bennett <stt.btt@gmail.com>
2024-01-21 17:43:52 -0500

    FEAT: Added symbol and color options for RatingsView

--- CustomRatings/ContentView.swift
+++ CustomRatings/ContentView.swift
@@ -9,11 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var currentRating = 0
+    @State private var heartRating = 0
+    @State private var pinRating = 0
+    
     var body: some View {
         RatingsView(
             maxRating: 5,
             currentRating: $currentRating,
             width: 40
+        )
+        RatingsView(
+            maxRating: 3,
+            currentRating: $heartRating,
+            width: 50,
+            symbol: "heart",
+            color: .red
+        )
+        RatingsView(
+            maxRating: 5,
+            currentRating: $pinRating,
+            width: 40,
+            symbol: "pin",
+            color: .blue
         )
     }
 }
--- CustomRatings/RatingsView.swift
+++ CustomRatings/RatingsView.swift
@@ -7,24 +7,60 @@
 
 import SwiftUI
 
+/// A view of inline images that represents a rating.
+/// Tapping on an image will change if from an unfilled to a filled version of the image.
+///
+/// The following example shows a Ratings view with a maximum rating of 3 red hearts
+/// bound to a heartRating property and a width of 50 specified.
+///
+///     RatingsView(
+///                 maxRating: 3,
+///                 currentRating: $heartRating,
+///                 width: 50,
+///                 symbol: "heart",
+///                 color: .red)
+///
+///
+
 struct RatingsView: View {
     
     let maxRating: Int
     @Binding var currentRating: Int
     let width: Int
+    let symbol: String
+    let color: Color
     
-    init(maxRating: Int, currentRating: Binding<Int>, width: Int = 30) {
+    /// Only two required parameters are maxRating and the binding to currentRating. All other parameters have default values
+    /// - Parameters:
+    ///    - maxRating:  The maximum rating on the scale
+    ///    - currentRating:  A binding to the current rating variable
+    ///    - width:  The width of the image used for the rating (Default - 20)
+    ///    - symbol:  A String representing an SFImage that has a slash and fill variant (Default - "star")
+    ///    - color: The color of the image (Default - .yellow)
+ 
+    
+         
+    
+    init(
+        maxRating: Int,
+        currentRating: Binding<Int>,
+        width: Int = 30,
+        symbol: String = "star",
+        color: Color = .yellow
+    ) {
         self.maxRating = maxRating
         self._currentRating = currentRating
         self.width = width
+        self.symbol = symbol
+        self.color = color
     }
     
     var body: some View {
         HStack {
-            Image(systemName: "star")
+            Image(systemName: symbol)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.yellow)
+                .foregroundStyle(color)
                 .symbolVariant(.slash)
                 .opacity(currentRating == 0 ? 0 : 1)
                 .onTapGesture {
@@ -33,10 +69,10 @@ struct RatingsView: View {
                     }
                 }
             ForEach(0..<maxRating, id: \.self) { rating in
-                Image(systemName: "star")
+                Image(systemName: symbol)
                     .resizable()
                     .scaledToFit()
-                    .foregroundStyle(.yellow)
+                    .foregroundStyle(color)
                     .symbolVariant(rating < currentRating ? .fill : .none)
                     .onTapGesture {
                         withAnimation {
